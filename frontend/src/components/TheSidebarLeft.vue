eb9ac6de3e728f9186617bc9794f464a8f2bcf01
Emmanuel <eumana@inboundcycle.com>
2022-11-26 14:23:18 +0100

    Final

--- frontend/src/components/TheSidebarLeft.vue
+++ frontend/src/components/TheSidebarLeft.vue
@@ -19,25 +19,25 @@ const eventStore = useEventStore()
           @click="eventStore.toggleEventCreateForm()"
           class="flex text-lg text-primary items-center p-3 border-b mb-2 border-b-slate-200 hover:bg-gray-100/50 rounded gap-4 cursor-pointer"
         >
-          <SquaresPlusIcon class="w-8 h-8 text-violet-400" />
+          <SquaresPlusIcon class="w-7 h-7 text-violet-400" />
           <a class="pointer-events-none" @click.prevent="">Add Event</a>
         </li>
         <li
           class="flex text-lg text-primary items-center p-3 hover:bg-gray-100/50 rounded gap-4 cursor-pointer"
         >
-          <RectangleStackIcon class="w-8 h-8 text-violet-400" />
+          <RectangleStackIcon class="w-7 h-7 text-violet-400" />
           <a @click.prevent="">Listing</a>
         </li>
         <li
           class="flex text-lg text-primary items-center p-3 hover:bg-gray-100/50 rounded gap-4 cursor-pointer"
         >
-          <HandThumbUpIcon class="w-8 h-8 text-violet-400" />
+          <HandThumbUpIcon class="w-7 h-7 text-violet-400" />
           <a @click.prevent="">Liked Events</a>
         </li>
         <li
           class="flex text-lg text-primary items-center p-3 hover:bg-gray-100/50 rounded gap-4 cursor-pointer"
         >
-          <CalendarDaysIcon class="w-8 h-8 text-violet-400" />
+          <CalendarDaysIcon class="w-7 h-7 text-violet-400" />
           <a @click.prevent="">My Events</a>
         </li>
       </ul>
--- frontend/src/components/TheSidebarRigh.vue
+++ frontend/src/components/TheSidebarRigh.vue
@@ -2,7 +2,24 @@
 
 <template>
   <aside class="sidebar sidebar-right">
-    <h2>Sidebar Right</h2>
+    <h2 class="mb-7">Our Sponsors</h2>
+
+    <div class="sponsors-list flex gap-6">
+      <figure class="w-2/4">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4E0BAQHEuqUhYgLIuA/company-logo_200_200/0/1600167351881?e=2147483647&v=beta&t=Cn04lAoB0zmkml1mX2lmA2rLbouQoEk7cMmu5yccg5E"
+          alt=""
+          class="h-full w-full object-cover"
+        />
+      </figure>
+      <figure class="w-2/4">
+        <img
+          src="https://cdn.smehost.net/2019sonymusiccom-smeprod/wp-content/uploads/2020/06/sony-music-large-logo.jpg"
+          alt=""
+          class="h-full w-full object-cover"
+        />
+      </figure>
+    </div>
   </aside>
 </template>
 
